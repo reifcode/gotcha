b58bab80c459ab90bfefbba5cf1e359b2519383f
Federico Ruggi <f.ruggi@icloud.com>
2017-08-15 23:50:24 +0200

    reordered methods

--- gotcha.go
+++ gotcha.go
@@ -41,6 +41,101 @@ func NewGotcha(cfg config.DefaultReporterConfigType) *Gotcha {
 	}
 }
 
+func (g *Gotcha) AnnounceSuite(description string, randomSeed int64, randomizingAll bool, quiet bool) {
+	fmt.Println()
+	fmt.Println(description)
+	fmt.Println()
+}
+
+func (g *Gotcha) AnnounceSuccesfulSpec(spec *types.SpecSummary) {
+	g.printSingleSpec(spec, "[OK] ", color.Green)
+}
+
+func (g *Gotcha) AnnounceSuccesfulSlowSpec(spec *types.SpecSummary, quiet bool) {
+	g.printSingleSpec(spec, "[OK] ", color.Green)
+}
+
+func (g *Gotcha) AnnounceSuccesfulMeasurement(spec *types.SpecSummary, quiet bool) {
+	g.printSingleSpec(spec, "[OK] ", color.Green)
+}
+
+func (g *Gotcha) AnnouncePendingSpec(spec *types.SpecSummary, verbose bool) {
+	g.printSingleSpec(spec, "[PENDING] ", color.Yellow)
+}
+
+func (g *Gotcha) AnnounceSpecTimedOut(spec *types.SpecSummary, quiet bool, fullTrace bool) {
+	g.printSingleSpec(spec, "[FAIL] ", color.Red)
+}
+
+func (g *Gotcha) AnnounceSpecPanicked(spec *types.SpecSummary, quiet bool, fullTrace bool) {
+	g.printSingleSpec(spec, "[FAIL] ", color.Red)
+}
+
+func (g *Gotcha) AnnounceSpecFailed(spec *types.SpecSummary, quiet bool, fullTrace bool) {
+	g.printSingleSpec(spec, "[FAIL] ", color.Red)
+}
+
+func (g *Gotcha) AnnounceSpecRunCompletion(summary *types.SuiteSummary, quiet bool) {
+	fmt.Println()
+	fmt.Println(fmt.Sprintf("Finished in %.4f seconds", summary.RunTime.Seconds()))
+
+	var fn colorFunc
+	if summary.NumberOfFailedSpecs > 0 {
+		fn = color.Red
+	} else if summary.NumberOfPassedSpecs == summary.NumberOfTotalSpecs {
+		fn = color.Green
+	} else {
+		fn = color.Yellow
+	}
+	g.printSummary(summary, fn)
+
+	fmt.Println()
+}
+
+func (g *Gotcha) SummarizeFailures(summaries []*types.SpecSummary) {
+	var failures []*types.SpecSummary
+	for i := range summaries {
+		if summaries[i].HasFailureState() {
+			failures = append(failures, summaries[i])
+		}
+	}
+	if len(failures) > 0 {
+		fmt.Println()
+		fmt.Println("Failures:")
+		fmt.Println()
+
+		for i, failed := range failures {
+			failure := failed.Failure
+			fmt.Printf("  %d) %s\n", i+1, strings.Join(failed.ComponentTexts[1:], " "))
+			lines := strings.Split(failure.Message, "\n")
+			for i := range lines {
+				lines[i] = fmt.Sprintf("     %s", lines[i])
+			}
+			color.Red(strings.Join(lines, "\n"))
+			color.Cyan(fmt.Sprintf("     %s", failure.Location.String()))
+			fmt.Println()
+		}
+	}
+
+	var pendings []*types.SpecSummary
+	for i := range summaries {
+		if summaries[i].Pending() {
+			pendings = append(pendings, summaries[i])
+		}
+	}
+	if len(pendings) > 0 {
+		fmt.Println()
+		fmt.Println("Pending:")
+		fmt.Println()
+
+		for i, pending := range pendings {
+			fmt.Println(fmt.Sprintf("  %d) %s", i+1, strings.Join(pending.ComponentTexts[1:], " ")))
+			loc := pending.ComponentCodeLocations[len(pending.ComponentCodeLocations)-1]
+			color.Cyan(fmt.Sprintf("     %s", loc))
+		}
+	}
+}
+
 type colorFunc func(string, ...interface{})
 
 func (g *Gotcha) printSingleSpec(spec *types.SpecSummary, prefix string, fn colorFunc) {
@@ -91,101 +186,6 @@ func (g *Gotcha) printSummary(summary *types.SuiteSummary, fn colorFunc) {
 		fn(line)
 	} else {
 		fmt.Println(line)
-	}
-}
-
-func (g *Gotcha) AnnounceSuite(description string, randomSeed int64, randomizingAll bool, quiet bool) {
-	fmt.Println()
-	fmt.Println(description)
-	fmt.Println()
-}
-
-func (g *Gotcha) AnnounceSpecRunCompletion(summary *types.SuiteSummary, quiet bool) {
-	fmt.Println()
-	fmt.Println(fmt.Sprintf("Finished in %.4f seconds", summary.RunTime.Seconds()))
-
-	var fn colorFunc
-	if summary.NumberOfFailedSpecs > 0 {
-		fn = color.Red
-	} else if summary.NumberOfPassedSpecs == summary.NumberOfTotalSpecs {
-		fn = color.Green
-	} else {
-		fn = color.Yellow
-	}
-	g.printSummary(summary, fn)
-
-	fmt.Println()
-}
-
-func (g *Gotcha) AnnounceSuccesfulSpec(spec *types.SpecSummary) {
-	g.printSingleSpec(spec, "[OK] ", color.Green)
-}
-
-func (g *Gotcha) AnnounceSuccesfulSlowSpec(spec *types.SpecSummary, quiet bool) {
-	g.printSingleSpec(spec, "[OK] ", color.Green)
-}
-
-func (g *Gotcha) AnnounceSuccesfulMeasurement(spec *types.SpecSummary, quiet bool) {
-	g.printSingleSpec(spec, "[OK] ", color.Green)
-}
-
-func (g *Gotcha) AnnouncePendingSpec(spec *types.SpecSummary, verbose bool) {
-	g.printSingleSpec(spec, "[PENDING] ", color.Yellow)
-}
-
-func (g *Gotcha) AnnounceSpecTimedOut(spec *types.SpecSummary, quiet bool, fullTrace bool) {
-	g.printSingleSpec(spec, "[FAIL] ", color.Red)
-}
-
-func (g *Gotcha) AnnounceSpecPanicked(spec *types.SpecSummary, quiet bool, fullTrace bool) {
-	g.printSingleSpec(spec, "[FAIL] ", color.Red)
-}
-
-func (g *Gotcha) AnnounceSpecFailed(spec *types.SpecSummary, quiet bool, fullTrace bool) {
-	g.printSingleSpec(spec, "[FAIL] ", color.Red)
-}
-
-func (g *Gotcha) SummarizeFailures(summaries []*types.SpecSummary) {
-	var failures []*types.SpecSummary
-	for i := range summaries {
-		if summaries[i].HasFailureState() {
-			failures = append(failures, summaries[i])
-		}
-	}
-	if len(failures) > 0 {
-		fmt.Println()
-		fmt.Println("Failures:")
-		fmt.Println()
-
-		for i, failed := range failures {
-			failure := failed.Failure
-			fmt.Printf("  %d) %s\n", i+1, strings.Join(failed.ComponentTexts[1:], " "))
-			lines := strings.Split(failure.Message, "\n")
-			for i := range lines {
-				lines[i] = fmt.Sprintf("     %s", lines[i])
-			}
-			color.Red(strings.Join(lines, "\n"))
-			color.Cyan(fmt.Sprintf("     %s", failure.Location.String()))
-			fmt.Println()
-		}
-	}
-
-	var pendings []*types.SpecSummary
-	for i := range summaries {
-		if summaries[i].Pending() {
-			pendings = append(pendings, summaries[i])
-		}
-	}
-	if len(pendings) > 0 {
-		fmt.Println()
-		fmt.Println("Pending:")
-		fmt.Println()
-
-		for i, pending := range pendings {
-			fmt.Println(fmt.Sprintf("  %d) %s", i+1, strings.Join(pending.ComponentTexts[1:], " ")))
-			loc := pending.ComponentCodeLocations[len(pending.ComponentCodeLocations)-1]
-			color.Cyan(fmt.Sprintf("     %s", loc))
-		}
 	}
 }
 
